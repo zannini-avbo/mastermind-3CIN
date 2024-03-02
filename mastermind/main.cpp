b9cbe507a476ee2e73ba5060f03b5d9b1a782ffc
Matteo Zannini <144007002+zannini-avbo@users.noreply.github.com>
2024-03-02 12:02:11 +0000

    soluzione

--- mastermind/main.cpp
+++ mastermind/main.cpp
@@ -1,11 +1,21 @@
+#include <iostream>
+
 #include "mastermind.cpp"
 
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     mastermind prova = mastermind();
     
-    prova.nuova_giocata();
+    cout << "Benvenuto al gioco del mastermind!" << endl;
+    do{
+        prova.nuova_giocata();
+        prova.risultato_mossa();
+    }while (prova.puoi_continuare());
+    
+    prova.situazione_finale();
 
     return 0;
 }
--- mastermind/mastermind.cpp
+++ mastermind/mastermind.cpp
@@ -5,12 +5,15 @@
 class mastermind
 {
 private:
+    static const int NUMERO_MOSSE_CONSENTITE = 9;
     int numero_mosse;
     std::string ultima_mossa;
 
     static const int DIM_GIOCATA_VALIDA = 4;
     int mossa_valida[DIM_GIOCATA_VALIDA];
     int codice_segreto[DIM_GIOCATA_VALIDA];
+
+    bool hai_vinto;
 
     bool sanifica_input(){
         if (DIM_GIOCATA_VALIDA != ultima_mossa.size())
@@ -42,6 +45,7 @@ public:
     mastermind(){
         numero_mosse = 0;
         ultima_mossa = "";
+        hai_vinto = false;
         genera_codice_segreto();
     };
 
@@ -58,10 +62,44 @@ public:
         int num_ball = 0;
         int num_strike = 0;
 
-        // ... code here ...
+        for (int i = 0; i < DIM_GIOCATA_VALIDA; i++)
+        {
+            for (int j = 0; j < DIM_GIOCATA_VALIDA; j++)
+            {
+                if(codice_segreto[i] == mossa_valida[j]){
+                    if(i == j)
+                        num_strike++;
+                    else
+                        num_ball++;
+                }
+            }
+        }
+
+        if(num_strike == DIM_GIOCATA_VALIDA)
+            hai_vinto = true;        
 
         std::cout << "ball: " << num_ball << std::endl;
         std::cout << "strike: " << num_strike << std::endl;
+    }
+
+    bool puoi_continuare(){
+        if(hai_vinto || numero_mosse >= NUMERO_MOSSE_CONSENTITE)
+            return false;
+        return true;
+    }
+
+    void situazione_finale(){
+        if(hai_vinto)
+            std::cout << "Hai vinto in " << numero_mosse << " mosse!" << std::endl;
+        else{
+            std::cout << "Hai perso, il codice segreto e': ";
+            for (id_t i = 0; i < DIM_GIOCATA_VALIDA; i++)
+            {
+                std::cout << codice_segreto[i];
+            }
+            std::cout << std::endl;
+        }
+            
     }
 
 
